d62fc84814466c93677adedd08d6e3e360027b88
Tillymolete <mahlatsemolete@gmail.com>
2024-01-19 06:45:58 -0800

    creatingapupfile

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-#create /tmp/school files with specified requirements
-file {'/tmp/holberton':
-  ensure  => 'file',
-  content => 'I love Puppet',
+# Puppet manifest to create a file in /tmp
+
+file {'/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    =>  '0744',
+  content => 'I love Puppet'
 }
