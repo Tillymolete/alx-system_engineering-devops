e712dc325e7c786fe9d114dc5246bf7a61522787
Tillymolete <mahlatsemolete@gmail.com>
2024-01-19 07:08:11 -0800

    killscommand

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
-#Kills the process named "killmenow" using pkill
-exec { 'killmenow':
+# Puppet manifest that executes a kill command
+
+exec {'killmenow':
+  path    => '/usr/bin/:/usr/sbin:/bin',
   command => 'pkill -f killmenow',
-  path    => '/usr/bin',
 }
